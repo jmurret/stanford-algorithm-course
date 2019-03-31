143039a80232e2dcb5d60eb0bfa28b2f85510cea
John Murret <jmurret@xactlycorp.com>
2019-03-31 09:25:44 -0600

    simplify merge sort

--- src/sorting/mergeSort.js
+++ src/sorting/mergeSort.js
@@ -7,42 +7,33 @@ const mergeSort = (arr, l = 0, r = arr.length - 1) => {
 };
 
 const merge = (arr, l, m, r) => {
-  let n1 = m - l + 1;
-  let n2 =  r - m;
+  let nLeft = m - l + 1;
+  let nRight =  r - m;
 
+  /* copy items into temp arrays */
   const tempL = [], tempR= [];
-  for (let i = 0; i < n1; i++)
+  for (let i = 0; i < nLeft; i++)
       tempL[i] = arr[l + i];
-  for (let j = 0; j < n2; j++)
+  for (let j = 0; j < nRight; j++)
       tempR[j] = arr[m + 1+ j];
 
+  /* merge items back from temp arrays into original */
   let k = l, i = 0, j = 0;
-  while (i < n1 && j < n2)
+  while (i < nLeft || j < nRight)
   {
-      if (tempL[i] <= tempR[j])
-      {
+      if (j === nRight & i < nLeft) {
           arr[k] = tempL[i];
           i++;
-      }
-      else
-      {
+      } else if (i === nLeft & j < nRight) {
+          arr[k] = tempR[j];
+          j++;
+      } else if (tempL[i] <= tempR[j]) {
+          arr[k] = tempL[i];
+          i++;
+      } else {
           arr[k] = tempR[j];
           j++;
       }
-      k++;
-  }
-
-  while (i < n1)
-  {
-      arr[k] = tempL[i];
-      i++;
-      k++;
-  }
-
-  while (j < n2)
-  {
-      arr[k] = tempR[j];
-      j++;
       k++;
   }
 }
